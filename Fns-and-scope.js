46a5532bb00cf42cb49aac1e4bf95b2d305029c7
john <johnpwoodiv@gmail.com>
2017-08-25 14:35:57 -0500

    done

--- Fns-and-scope.js
+++ Fns-and-scope.js
@@ -1,18 +1,17 @@
 //////////////////PROBLEM 1////////////////////
 
+var name = 'Tyler';
 //Create a function called isTyler that accepts name as it's only parameter.
 //If the argument you passed in is equal to 'Tyler', return true. If it's not, return false.
 
   //Code Here
-  var isTyler = (x) => {return x === "Tyler"? true: false};
+  var isTyler = (x){x="Tyler"?true:false}
 //////////////////PROBLEM 2////////////////////
 
 
 //Create a function called getName that uses prompt() to prompt the user for their name, then returns the name.
 
-var getName = () => {return prompt(name);
 
-}
   //Code Here
 
 
@@ -25,7 +24,6 @@ var getName = () => {return prompt(name);
 
   //Code Here
 
-var welcome = () => {alert("Welcome, " + getName());};
 
 //////////////////PROBLEM 4////////////////////
 
@@ -35,8 +33,7 @@ var welcome = () => {alert("Welcome, " + getName());};
 //What is the difference between arguments and parameters?
 
   //Answer Here
-// Parameters are the variables that need to be passed as arguments when the function is called.
-// Arguments are the variables that are passed to fill the place of the parameters of the funcion.
+
 
 //////////////////PROBLEM 5////////////////////
 
@@ -46,7 +43,7 @@ var welcome = () => {alert("Welcome, " + getName());};
 
 
   //Answer Here
-//  0, "", undefined, false, nil, NaN
+
 
 
 //////////////////PROBLEM 6////////////////////
@@ -56,13 +53,13 @@ var welcome = () => {alert("Welcome, " + getName());};
 //Create a function called myName that returns your name
 
   //Code Here
-var myName = () => {return "John";};
+
 
 
 //Now save the function definition of myName into a new variable called newMyName
 
   //Code Here
-var newMyName = myName;
+
 //Now alert the result of invoking newMyName
 
 
@@ -74,9 +71,9 @@ var newMyName = myName;
 //Create a function called outerFn which returns an anonymous function which returns your name.
 
   //Code Here
-var outerFn = () => {return () => {return "John"}}
+
 //Now save the result of invoking outerFn into a variable called innerFn.
 
   //Code Here
-var innerFn = outerFn();
+
 //Now invoke innerFn.
